a814c7fe5fa08e34e9460000e9e876da160edd45
oncekiller <canard.antoine@gmail.com>
2025-02-20 08:01:30 +0100

    add technical dates AIContent

--- src/models/AIContent.ts
+++ src/models/AIContent.ts
@@ -13,4 +13,12 @@ export class AIContent {
 
   @Column({type: "text"})
   content?: string;
+
+  //Technical Date
+  @Column({ type: 'timestamp', default: () => 'NOW()' })
+  created_at!: Date;
+
+  //Technical Date
+  @Column({ type: 'timestamp', default: () => 'NOW()', onUpdate: 'CURRENT_TIMESTAMP' })
+  updated_at!: Date;
 }
